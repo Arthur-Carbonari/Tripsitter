eb615614931d5f441a345264d12c249c8bdba9d0
Arthur Martins <arthurcarbonari99@gmail.com>
2023-03-29 18:56:32 -0300

    Connect mainRouter to the server

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express"
 import { engine } from 'express-handlebars';
 
+import mainRouter from "./routes/mainRouter.js";
+
 const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(mainRouter)
 
 app.listen(5000)
 
