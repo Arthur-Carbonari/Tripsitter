b6f3bc7b8e4213abf67feec88ee4285b2c70394a
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-03 00:42:54 -0300

    Update home router

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,17 @@
 import express from 'express';
 
+import Post from '../models/post.js';
+
 const homeRouter = express.Router();
 
-homeRouter.get('/', (req, res) => {
-    res.render('home', { title: 'My Blog', year: new Date().getFullYear() });
+homeRouter.get('/', async (req, res) => {
+    const posts = await Post.find().sort({ date: 'desc' }).limit(10).lean();
+    
+    posts.forEach( post => {
+        post.date = post.date.toLocaleString()
+    })
+
+    res.render('home', { title: 'My Blog', year: new Date().getFullYear(), posts: posts});
 });
 
 export default homeRouter;
