e4428d389207e0b535b2282efd900eea12e0aa36
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-03 00:29:58 -0300

    Connect app to mongodb

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 import express from "express"
 import { engine } from 'express-handlebars';
+import mongoose from "mongoose";
 
 import mainRouter from "./routes/mainRouter.js";
 
 const app = express();
 
-app.engine('.hbs', engine({extname: '.hbs'}));
+// Connect to MongoDB
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Fatal Error connecting to MongoDB:\n', error);
+        process.exit(1);
+    }
+};
+await connectToMongoDB();
+
+// Setup view engine handlebars
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+// Setup middleware
 app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Router
 app.use(mainRouter)
 
 app.listen(5000)
