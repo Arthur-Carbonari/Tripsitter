6f26141d8871084b6e414d4e90e35c47854cf924
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-06 03:02:15 -0300

    update main router to use middleware for protected routes

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,15 +1,23 @@
 import express from "express"
 
+// imports the sub routers
 import homeRouter from "./home.js"
 import blogRouter from "./blog.js"
 import authRouter from "./auth.js"
 import adminRouter from "./admin.js"
 
+// imports the middleware
+import authMiddleware from "../middleware/authMiddleware.js"
+
 const mainRouter = express.Router()
+
+mainRouter.use("/auth", authRouter)
+
+mainRouter.use(authMiddleware.authenticateUser)
 
 mainRouter.use("/", homeRouter)
 mainRouter.use("/blog", blogRouter)
-mainRouter.use("/auth", authRouter)
-mainRouter.use("/admin", adminRouter)
+
+mainRouter.use("/admin", authMiddleware.requireAdminAuth, adminRouter)
 
 export default mainRouter
