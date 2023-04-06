c653b04c56cae71fd65e7e061ca70c56d84b5eef
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-06 02:58:47 -0300

    Add auth controllers to auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,18 @@
 // routes/auth.js
 
 import express from 'express';
+
+import authController from '../controllers/authController.js';
+
 const authRouter = express.Router();
 
 // Route for displaying the user login/registration form and to login an existing user
-authRouter.route('/')
-    .get((req, res) => {
-        res.render('auth', { title: 'Login', year: new Date().getFullYear() });
-    })
-    .post((req, res) => {
-        // login user here
-    });
+authRouter.post('/login', authController.loginPost)
 
 // Post route to register a new user
-authRouter.post('/register', (req, res) => {
-});
+authRouter.post('/signup', authController.signupPost)
 
 // Route for handling user logout
-authRouter.get('/logout', (req, res) => {
-
-    // logout user here
-
-    res.redirect('/');
-});
+authRouter.get('/logout', authController.logoutGet)
 
 export default authRouter;
