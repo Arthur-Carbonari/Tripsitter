ad1c1e3654aada367ac99f164bd88b6e6079befe
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-06 02:57:07 -0300

    Add controllers to admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,43 +1,30 @@
 import express from 'express';
+
+import Post from '../models/post.js';
+import adminController from '../controllers/adminController.js';
+
 const adminRouter = express.Router();
 
 // Route for displaying the admin dashboard
-adminRouter.get('/', (req, res) => {
-    // Render the admin dashboard view
-    res.render('admin/dashboard', { title: 'Admin Dashboard' });
-});
+adminRouter.get('/', (req, res) => { res.render('admin/dashboard', { title: 'Admin Dashboard' }) });
 
 // Route for displaying the admin user control interface
 adminRouter.route('/users')
-    .get((req, res) => {
-        // Code to display the list of users goes here
-        res.render('admin/users', { title: 'Admin list of users' });
-    })
-    .post((req, res) => {
-        // Code to create a new user goes here
-    })
-    .put((req, res) => {
-        // Code to update a user goes here
-    })
-    .delete((req, res) => {
-        // Code to delete a user goes here
-    });
+    .get(adminController.usersGet)
+    .post(adminController.usersPost)
+    .patch(adminController.usersPatch)
+    .delete(adminController.usersDelete)
 
 // Route for displaying the admin posts control interface
 adminRouter.route('/posts')
-.get((req, res) => {
-    // Code to display the list of posts goes here
-    res.render('admin/posts', { title: 'Admin list of posts' });
-})
-.post((req, res) => {
-    // Code to create a new post goes here
-})
-.put((req, res) => {
-    // Code to update a post goes here
-})
-.delete((req, res) => {
-    // Code to delete a post goes here
-});
+    .get((req, res) => { res.render('admin/posts', { title: 'Admin list of posts' }) })
+    .post(adminController.postsPost)
+    .put((req, res) => {
+        // Code to update a post goes here
+    })
+    .delete((req, res) => {
+        // Code to delete a post goes here
+    });
 
 
 
