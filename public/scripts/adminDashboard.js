d54e96b5458b61354e4443d7a1c42fffee8228bc
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-07 04:15:54 -0300

    Rewrite adminDashboard to use SmartForm class to handle form operations

--- public/scripts/adminDashboard.js
+++ public/scripts/adminDashboard.js
@@ -1,200 +1,136 @@
-const registerUserModal = new bootstrap.Modal('#registerUserModal')
-const registerUserForm = document.getElementById('registerUserForm')
-const registerUserInputs = registerUserForm.querySelectorAll('.form-key')
+async function adminDashboardMain() {
 
-const updateUserModal = new bootstrap.Modal('#updateUserModal')
-const updateUserForm = document.getElementById('updateUserForm')
-const updateUserFields = updateUserForm.querySelectorAll('input')
+    const registerUserModal = $('#registerUserModal');
+    const registerUserForm = new SmartForm('registerUserForm')
 
-const deleteUserModal = new bootstrap.Modal('#deleteUserModal')
-const deleteUserForm = document.getElementById('deleteUserForm')
+    registerUserModal.on('hidden.bs.modal', () => { registerUserForm.resetForm() })
 
-let table
+    const updateUserModal = $('#updateUserModal');
+    const updateUserForm = new SmartForm('updateUserForm')
 
-fetch("/admin/users")
-    .then(response => response.json())
-    .then(data => {
-        $(document).ready(function () {
+    const deleteUserModal = $('#deleteUserModal')
+    const deleteUserForm = new SmartForm('deleteUserForm')
 
-            table = $('#usersTable').DataTable({
+    const res = await fetch("/admin/users")
+    const data = await res.json()
 
-                data: data,
-                columns: [
-                    { data: '_id', visible: false, searchable: false },
-                    { data: 'email' },
-                    { data: 'username' },
-                    { data: 'firstName' },
-                    { data: 'lastName' },
-                    { data: 'adminRights' },
-                    { data: '' }
-                ],
-                rowId: '_id',
-                columnDefs: [
-                    {
-                        targets: -1,
-                        data: null,
-                        defaultContent: '<div class="text-end"> <button class="btn btn-primary edit-button">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>' + 
-                                        '<button class="btn btn-danger delete-button">Delete <i class="fa fa-trash-o" aria-hidden="true"></i> </button> </div>',
-                    },
-                ],
-                pagingType: "first_last_numbers",
-                dom: "<'row align-items-center'<'col-sm-6 my-searchbox'f><'toolbar w-auto text-end flex-grow-1'>>" +
-                "<'row'<'col-sm-12'tr>>" +
-                "<'row justify-content-between pt-2'<'col-sm-3'l><'col-sm-5 sub-div-p-0 text-center'i><'col-sm-3'p>>",
-            });
 
-            $('div.toolbar').html('<button class="btn btn-primary" id="addUserButton" data-bs-toggle="modal" data-bs-target="#registerUserModal"> Add New User <i class="fa fa-plus" aria-hidden="true"></i> </button>');
+    table = $('#usersTable').DataTable({
 
-            $('#usersTable tbody').on('click', '.edit-button', function () {
-                let data = table.row($(this).parents('tr')).data();
+        data: data,
+        columns: [
+            { data: '_id', visible: false, searchable: false },
+            { data: 'email' },
+            { data: 'username' },
+            { data: 'firstName' },
+            { data: 'lastName' },
+            { data: 'adminRights' },
+            { data: '' , searchable: false}
+        ],
+        rowId: '_id',
+        columnDefs: [
+            {
+                targets: -1,
+                data: null,
+                defaultContent: '<div class="text-end"> <button class="btn btn-primary edit-button">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>' +
+                    '<button class="btn btn-danger delete-button">Delete <i class="fa fa-trash-o" aria-hidden="true"></i> </button> </div>',
+            },
+        ],
+        pagingType: "first_last_numbers",
+        dom: "<'row align-items-center'<'col-sm-6 my-searchbox'f><'toolbar w-auto text-end flex-grow-1'>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row justify-content-between pt-2'<'col-sm-3'l><'col-sm-5 sub-div-p-0 text-center'i><'col-sm-3'p>>",
+    });
 
-                updateUserFields.forEach( field => { 
-                    field.value = data[field.name]
-                    field.classList.remove('is-invalid')
-                })
-                updateUserForm.adminRights.checked = data.adminRights
+    $('div.toolbar').html('<button class="btn btn-primary" id="addUserButton" data-bs-toggle="modal" data-bs-target="#registerUserModal"> Create New <i class="fa fa-plus" aria-hidden="true"></i> </button>');
 
-                updateUserModal.show()
-            });
+    $('#usersTable tbody').on('click', '.edit-button', function () {
+        let data = table.row($(this).parents('tr')).data();
 
-            $('#usersTable tbody').on('click', '.delete-button', function () {
-                let data = table.row($(this).parents('tr')).data();
+        updateUserForm.setData(data)
+        updateUserForm.form.adminRights.checked = data.adminRights
 
-                deleteUserForm._id.value = data._id
-                deleteUserForm.email.value = data.email
-                deleteUserForm.username.value = data.username
+        updateUserModal.modal('show')
+    });
 
-                deleteUserModal.show()
-            });
+    $('#usersTable tbody').on('click', '.delete-button', function () {
+        let data = table.row($(this).parents('tr')).data();
 
-        });
-    })
+        deleteUserForm.setData(data)
 
-registerUserForm.addEventListener('submit', async (e) => {
+        deleteUserModal.modal('show')
+    });
 
-    e.preventDefault()
 
-    if (registerUserForm.password.value != registerUserForm.confirmPassword.value) {
-        registerUserForm.password.classList.add('is-invalid')
-        registerUserForm.password.nextElementSibling.innerHTML = "Password fields do not match"
-        return
-    }
+    registerUserForm.overwriteSubmitEvent(async (e) => {
 
-    const newUser = {}
-    registerUserInputs.forEach( input => {
-        input.classList.remove('is-invalid')
-
-        newUser[input.name] = input.value 
-    })
-    newUser.adminRights = registerUserForm.adminRights.checked
-
-    try {
-        const res = await fetch('/admin/users', {
-            method: 'POST',
-            body: JSON.stringify(newUser),
-            headers: { 'Content-Type': "application/json" }
-        })
-
-        const data = await res.json()
-
-        // request was a sucess and user was created go to the main page
-        if (data['sucess']) {
-
-            table.row.add(data.newUser).draw()
-            registerUserModal.hide()
-
+        if (registerUserForm.getFieldValue('password') != registerUserForm.getFieldValue('confirmPassword')) {
+            registerUserForm.addWarnings({ password: { message: "Password fields do not match" } })
+            return
         }
 
-        // request wasnt a sucess, we check response object for what fields have an error message and add the is-invalid class and the error message to them
-        addErrorsToInputFields(registerUserInputs, data)
+        registerUserForm.clearWarnings()
 
-    } catch (err) {
-        console.log(err)
-    }
+        registerUserForm.form.adminRights.value = registerUserForm.form.adminRights.checked
 
-})
+        try {
+            const resData = await registerUserForm.submitForm('/admin/users', 'POST')
 
-document.getElementById('registerUserModal').addEventListener('hidden.bs.modal', function (event) {
-    
-    registerUserInputs.forEach( field => {
-        field.value = null
-    })
+            // request was a sucess and user was created go to the main page
+            if (resData.sucess) {
 
-    registerUserForm.confirmPassword.value = null
+                table.row.add(resData.newUser).draw()
+                registerUserModal.modal('hide')
 
-});
+            }
 
-updateUserForm.addEventListener('submit', async (e) => {
-    
-    e.preventDefault()
-
-    const object = {}
-    updateUserFields.forEach(field => {
-        object[field.name] = field.value
-    })
-
-    object.adminRights = updateUserForm.adminRights.checked
-
-    try {
-        const res = await fetch('/admin/users', {
-            method: 'PATCH',
-            body: JSON.stringify(object),
-            headers: { 'Content-Type': "application/json" }
-        })
-
-        const data = await res.json()
-
-        // request was a sucess and user was created
-        if (data['sucess']) {
-
-            table.row('#' + data.user._id).data(data.user).draw()
-
-            return updateUserModal.hide()
+        } catch (err) {
+            console.log(err)
         }
 
+    })
 
-        // request wasnt a sucess, we add the respective errors to theirs input field
-        addErrorsToInputFields(updateUserFields, data)
+    updateUserForm.overwriteSubmitEvent(async (e) => {
 
-    } catch (err) {
-        console.log(err)
-    }       
+        updateUserForm.clearWarnings()
 
-})
+        updateUserForm.form.adminRights.value = updateUserForm.form.adminRights.checked
 
-deleteUserForm.addEventListener('submit', async (e) => {
-    e.preventDefault()
+        try {
 
-    const _id = deleteUserForm._id.value
+            const resData = await updateUserForm.submitForm('/admin/users', 'PATCH')
 
-    try {
-        const res = await fetch('/admin/users', {
-            method: 'DELETE',
-            body: JSON.stringify({_id}),
-            headers: { 'Content-Type': "application/json" }
-        })
+            // request was a sucess and user was created
+            if (resData.sucess) {
 
-        const data = await res.json()
+                table.row('#' + resData.user._id).data(resData.user).draw()
 
-        // request was a sucess and user was deleted, so we delete him from the table
-        if (data['sucess']) table.row('#' + _id).remove().draw()
-        
-        return deleteUserModal.hide()
+                return updateUserModal.modal('hide')
+            }
 
-    } catch (err) {
-        console.log(err)
-    }     
 
-})
-
-function addErrorsToInputFields(inputFields, errors){
-
-    inputFields.forEach( field => {
-
-        if(errors[field.name]){
-            field.classList.add('is-invalid')
-            field.nextElementSibling.innerHTML = errors[field.name].message
+        } catch (err) {
+            console.log(err)
         }
+
+    })
+
+    deleteUserForm.overwriteSubmitEvent(async (e) => {
+
+        try {
+            const resData = await deleteUserForm.submitForm('/admin/users', 'DELETE')
+
+            // request was a sucess and user was deleted, so we delete him from the table
+            if (resData.sucess) table.row('#' + resData.userId).remove().draw()
+
+            return deleteUserModal.modal('hide')
+
+        } catch (err) {
+            console.log(err)
+        }
+
     })
 
 }
+
+window.onload = adminDashboardMain
