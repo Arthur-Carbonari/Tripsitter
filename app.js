93aa7612cb7eab59f725d0fbb812ca8163553771
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-11 20:30:33 -0300

    changed version

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'
+dotenv.config() // configures the .env variables
+
 import express from "express"
 import { engine } from 'express-handlebars';
 import mongoose from "mongoose";
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config() // configures the .env variables
 
 import jwt from 'jsonwebtoken'
 
